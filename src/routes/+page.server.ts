6fd26b72314c2701e331a48c0bbc6097d09fd168
Artemy Egorov <artegoser@gmail.com>
2026-01-04 20:53:31 +0300

    fix package json reading in demo

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,6 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
+import pkg from '../../package.json' with { type: 'json' };
 
 export async function load() {
-	const __dirname = fileURLToPath(new URL('.', import.meta.url));
-	const packagePath = join(__dirname, '..', '..', 'package.json');
-	const pkg = JSON.parse(readFileSync(packagePath, 'utf-8'));
-
 	return {
 		version: pkg.version
 	};
